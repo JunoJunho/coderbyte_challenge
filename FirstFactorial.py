431eb106a668aed2cea65a572a6248cc6a8483a5
JunoJunho <wnsgh611@gmail.com>
2016-11-06 00:08:55 +0900

    FirstFactorial Solution

---
+++ FirstFactorial.py
@@ -0,0 +1,13 @@
+# FirstFractal
+# Using the Python language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
+
+def FirstFactorial(num): 
+    # code goes here 
+    num = int(num)
+    if num == 1:
+    	return 1
+    return num * FirstFactorial(num-1)
+    
+# keep this function call here  
+print FirstFactorial(raw_input())  
+
