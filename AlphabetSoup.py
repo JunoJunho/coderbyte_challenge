c272116b6b64e4cfb91ded7a614e7e072c77bb8c
JunoJunho <wnsgh611@gmail.com>
2016-11-14 13:26:47 +0900

    [Awesome] AlphabetSoup Solution

---
+++ AlphabetSoup.py
@@ -0,0 +1,12 @@
+# AlphabetSoup.py
+# Using the Python language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+
+
+def AlphabetSoup(str):
+
+    # code goes here
+    return ''.join(sorted(str))
+
+
+# keep this function call here
+print AlphabetSoup(raw_input())
